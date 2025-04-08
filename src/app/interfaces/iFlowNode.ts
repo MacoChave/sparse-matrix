cea254ed72cb88e81c4e5b89a3af6dbf042cce03
MacoChave <201020831@ingenieria.usac.edu.gt>
2025-04-07 22:20:55 -0600

    Add support to node type choose and clear nodes
    Changes to be committed:
    	modified:   src/app/flow-diagram/flow-diagram.component.css
    	modified:   src/app/flow-diagram/flow-diagram.component.html
    	modified:   src/app/flow-diagram/flow-diagram.component.ts
    	modified:   src/app/interfaces/iFlowNode.ts
    	new file:   src/app/new-flow-node/new-flow-node.component.css
    	new file:   src/app/new-flow-node/new-flow-node.component.html
    	new file:   src/app/new-flow-node/new-flow-node.component.ts

--- src/app/interfaces/iFlowNode.ts
+++ src/app/interfaces/iFlowNode.ts
@@ -1,20 +1,30 @@
+export enum FlowNodeType {
+  START = 'start',
+  END = 'end',
+  ADD = 'add',
+  IF = 'if',
+  THEN = 'then',
+  ELSE = 'else',
+  CONDITION = 'condition',
+  PROCESS = 'process',
+  IMPRIME = 'imprime',
+  NOTIFICA = 'notifica',
+  // Otros tipos de nodos
+  // ...
+}
+
 export interface iFlowNode {
   id: string;
   label: string;
-  type: 'start' | 'add' | 'then' | 'else' | 'end' | 'action' | 'condition' | 'if' | 'input' | 'output'; // Tipo de nodo
+  type: FlowNodeType; // Tipo de nodo (start, end, add, etc.)
   position: {
     x: number;
     y: number;
   };
-  hasRight?: boolean; // Para nodos hijos
-  hasBottom?: boolean; // Para nodos hijos
-  hasParent?: boolean; // Para nodos hijos
-
   data?: any; // Para almacenar datos adicionales
-  bottom?: iFlowNode; // Para nodos hijos
-  right?: iFlowNode; // Para nodos hijos
   parent?: iFlowNode; // Para nodos hijos
-  branches?: { true: string; false: string }; // Para condiciones
+  rightNode?: iFlowNode; // Para nodos hijos a la derecha
+  belowNode?: iFlowNode; // Para nodos hijos abajo
 }
 
 export interface iFlowLink {
